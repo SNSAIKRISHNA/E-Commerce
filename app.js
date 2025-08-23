b4cd8351cf19787eec48fa87725df2f92c104739
SNSAIKRISHNA <saikrishnasnoff@gmail.com>
2025-08-23 12:45:31 +0530

    Initial commit

---
+++ app.js
@@ -0,0 +1,19 @@
+const express = require('express');
+const dotenv = require('dotenv');
+const path= require('path');
+const app = express();
+dotenv.config({path: path.join(__dirname, 'config', 'config.env')});
+const connectDatabase =require('./config/connectDataBase')
+
+const products= require('./routes/product');
+const orders= require('./routes/order');
+
+connectDatabase();
+
+app.use('/api/v1/',products);
+app.use('/api/v1/',orders);
+
+
+app.listen(process.env.PORT,() =>{
+    console.log(`server listening to Port ${process.env.PORT} in ${process.env.NODE_ENV}`)
+});
---
+++ controller/productsController.js
@@ -0,0 +1,14 @@
+exports.getProducts =(req, res, next) => {
+    res.json({
+        success:true,
+        message: 'Get Products working'
+    })
+
+}
+exports.getSingleProducts =(req, res, next) => {
+    res.json({
+        success:true,
+        message: 'Get Single Products working'
+    })
+
+}
