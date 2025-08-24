995d5524f7c4dbf78764aef0b2d22b9a4eb7f482
SNSAIKRISHNA <saikrishnasnoff@gmail.com>
2025-08-24 11:53:51 +0530

    build products api  and testing

---
+++ models/productModel.js
@@ -0,0 +1,26 @@
+const mongoose  = require('mongoose');
+
+const productSchema = mongoose.Schema(
+    {
+        name:String,
+        price: String,
+        description:String,
+        ratings: String,
+        images: [
+            {
+                image: String
+            }
+        ],
+        category:String,
+        seller: String,
+        stock: String,
+        numOfReviews: String,
+        createdAt: Date
+
+    }
+)
+
+const productModel = mongoose.model('Product', productSchema);
+
+
+module.exports = productModel;
